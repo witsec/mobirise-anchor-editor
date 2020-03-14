67b8147f05f963f742e78d3fac8d1db3cad3c844
Mark <mark@witsec.nl>
2020-03-14 08:49:21 +0100

    Fixed compatibility with Mobirise v5

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-(function(jQuery, mbrApp) {
+defineM("witsec-anchor-editor", function(g, mbrApp, tr) {
 
 	var curr, compIndex;
     mbrApp.regExtension({
@@ -10,7 +10,7 @@
 					var h = jQuery(a);
 
 					// Add edit button to component buttons
-					var btn = '<span class="mbr-btn mbr-btn-default mbr-icon-link witsec-anchor-editor-shortcut" data-tooltipster="bottom" title="Edit Anchor"></span><style>.witsec-anchor-editor-shortcut:hover { background-color: #42a5f5 !important; }</style>';
+					var btn = '<span class="mbr-btn mbr-btn-default mbr-icon-link witsec-anchor-editor-shortcut" data-tooltipster="bottom" title="Edit Anchor"></span><style>.witsec-anchor-editor-shortcut:hover { background-color: #42a5f5 !important; color: #fff !important }</style>';
 					if (h.find(".component-params").length)
 						h.find(".component-params").before(btn);
 					else if (h.find(".component-remove").length)
@@ -57,35 +57,43 @@
 						body: [
 							'<div>Anchor: <input type="text" class"form-control" id="witsec-anchor-editor-name" value="', curr._anchor, '"></div>'
 						].join("\n"),
-						buttons: [{
-							label: "SAVE",
-							default: !0,
-							callback: function() {
-								try {
-									// Replace any whitespace characters with a dash, any other character is allowed in HTML5
-									var anchor = $('#witsec-anchor-editor-name').val().replace(/\s/g, "-");
-
-									// Check if the entered value isn't empty
-									if (anchor.trim() == "") {
-										mbrApp.alertDlg("Please give the anchor a value.");
-										return false;
-									}
-
-									// Replace spaces with dashes
-									anchor = anchor.replace(/\s/g , "-");
-
-									// Save the anchor
-									curr._anchor = anchor;
-									mbrApp.runSaveProject();
+						buttons: [
+							{
+								label: "CANCEL",
+								default: !1,
+								callback: function() {
 								}
-								catch(err){
-									mbrApp.alertDlg(err.name + ': ' + err.message);
+							},
+							{
+								label: "SAVE",
+								default: !0,
+								callback: function() {
+									try {
+										// Replace any whitespace characters with a dash, any other character is allowed in HTML5
+										var anchor = $('#witsec-anchor-editor-name').val().replace(/\s/g, "-");
+
+										// Check if the entered value isn't empty
+										if (anchor.trim() == "") {
+											mbrApp.alertDlg("Please give the anchor a value.");
+											return false;
+										}
+
+										// Replace spaces with dashes
+										anchor = anchor.replace(/\s/g , "-");
+
+										// Save the anchor
+										curr._anchor = anchor;
+										mbrApp.runSaveProject();
+									}
+									catch(err){
+										mbrApp.alertDlg(err.name + ': ' + err.message);
+									}
 								}
 							}
-						}]
+						]
 					});
 				});				
 			}
         }
     })
-})(jQuery, mbrApp);
+}, ["jQuery", "mbrApp", "TR()"]);
